b5d480fa2bbe209028dbf8459902e3bd09198c67
Andre Tran <atran03@wm.edu>
2023-07-13 13:33:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var pdfjs = document.createElement("script");
+pdfjs.setAttribute(
+  "src",
+  "https://cdn.jsdelivr.net/npm/pdfjs-dist@3.8.162/build/pdf.min.js"
+);
+document.head.appendChild(pdfjs);
+console.log("appended");
